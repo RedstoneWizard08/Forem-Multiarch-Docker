8c4c0bd2967a686c4e21c7528ee57b531eca7776
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-04-19 21:32:41 +0000

    Things

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-docker buildx build -t redstonewizard/forem:ruby -f Dockerfile \
+docker buildx build -t redstonewizard/forem:ruby-ubuntu -f Dockerfile.ubuntu \
     --progress plain --target ruby --load . --cache-from=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache \
     --cache-to=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache
 
-docker buildx build -t redstonewizard/forem:development -f Dockerfile \
+docker buildx build -t redstonewizard/forem:development-ubuntu -f Dockerfile.ubuntu \
     --progress plain --target development --load . --cache-from=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache \
     --cache-to=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache
 
-docker buildx build -t redstonewizard/forem:production -f Dockerfile \
+docker buildx build -t redstonewizard/forem:production-ubuntu -f Dockerfile.ubuntu \
     --progress plain --target production --load . --cache-from=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache \
     --cache-to=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache
 
-docker buildx build -t redstonewizard/forem:testing -f Dockerfile \
+docker buildx build -t redstonewizard/forem:testing-ubuntu -f Dockerfile.ubuntu \
     --progress plain --target testing --load . --cache-from=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache \
     --cache-to=type=registry,ref=docker.kadaroja.com/redstonewizard08/forem-cache
 
